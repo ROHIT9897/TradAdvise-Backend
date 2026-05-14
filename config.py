24e66b8c4022a5c55578bf4e398eba5ba50b1dc3
Rohit Sanger <rohitsanger1893@gmail.com>
2026-05-14 15:11:00 +0530

    updated config file

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
 
     class Config:
         env_file = ".env"
-	env_file_encoding = "utf-8"
-	case_senesitive = True
+	    env_file_encoding = "utf-8"
+	    case_senesitive = True
 
 settings = Settings()
